da49eaa241088bcc1b4bef25245e4af39dfcf2c8
Frederik Ring <frederik.ring@gmail.com>
2018-07-23 21:27:11 +0200

    add handler code and basic README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mongo-lambda-backup
-Backup MongoDB databases using AWS Lambda functions
+> Backup MongoDB databases using AWS Lambda functions
+
+The code in this repo allows running the backup of a MongoDB database to S3 on AWS Lambda by emulating a `mongodump` using `pymongo` and `boto3`. By default the function will create a JSON file for every collection in the database and save them in a `/backup` folder.
+
+All configuration values are passed using environment variables.
+
+The following values are required:
+
+- `MONGO_URI`: The URI including authentication credentials of the MongoDB host to be backed up.
+- `BUCKET_NAME`: The name of the S3 bucket to store the backup files in.
+
+The following values are optional:
+
+- `MONGO_DATABASE`: The name of the database to back up. In case this is not set, it will be read from the path of `MONGO_URI`.
+- `BUCKET_FOLDER`: The folder in the bucket to store the JSON files in. Defaults to `backups`.
+- `COLLECTION_BLACKLIST`: A comma-separated collection of collection names to skip when performing the back up (e.g. for skipping indices).
+
+### License
+MIT © [Frederik Ring](http://www.frederikring.com)
---
+++ main.py
@@ -0,0 +1,61 @@
+from os import environ
+from urllib.parse import urlparse
+import logging
+
+from pymongo import MongoClient
+import boto3 as boto
+from bson.json_util import dumps
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
+s3 = boto.resource("s3")
+
+
+def handler(event, context):
+    """
+    Perform a complete backup of a MongoDB database to a S3 bucket. Each collection
+    will be stored in a separate JSON file, as output by `mongodump`.
+    Required environment values are MONGO_URI, BUCKET_NAME. Optionally
+    MONGO_DATABASE, BUCKET_FOLDER and COLLECTION_BLACKLIST can be set.
+    """
+
+    temp_filepath = "/tmp/mongodump.json"
+    collection_blacklist = environ.get("COLLECTION_BLACKLIST")
+    bucket_folder = environ.get("BUCKET_FOLDER", "backups")
+    bucket_name = environ["BUCKET_NAME"]
+    db_uri = environ["MONGO_URI"]
+
+    db_name = environ.get("MONGO_DATABASE")
+    if db_name is None:
+        loc = urlparse(db_uri)
+        db_name = loc.path.strip("/")
+
+    LOGGER.info(
+        "Backing up collections from database %s in bucket %s", db_name, bucket_name
+    )
+
+    s3.meta.client.head_bucket(
+        Bucket=bucket_name
+    )  # Check that the given bucket actually exists.
+
+    client = MongoClient(db_uri)
+    database = client.get_database(db_name)
+
+    skip = (
+        [s.strip() for s in collection_blacklist.split(",")]
+        if collection_blacklist
+        else []
+    )
+    eligible_collections = [
+        name for name in database.list_collection_names() if not name in skip
+    ]
+
+    for collection_name in eligible_collections:
+        with open(temp_filepath, "w") as f:
+            for doc in database.get_collection(collection_name).find():
+                f.write(dumps(doc) + "\n")
+
+        s3.Bucket(bucket_name).upload_file(
+            temp_filepath, "{}/{}.json".format(bucket_folder, collection_name)
+        )
